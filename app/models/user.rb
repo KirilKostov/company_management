a5c00be6d1b96c87c274c2603d977c6ccebe23a7
KirilKostov <kiril_lol@abv.bg>
2013-06-19 15:36:29 +0300

    Add length and uniqueness validations for name, surname, password, and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,22 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :surname
-  validates :name, :surname, :email, :password, :presence => true
+
+  # Validation
+  validates :name, 
+            :presence => true,
+            :length   => { :within => 2..16 }
+
+  validates :surname, 
+            :presence => true,
+            :length   => { :within => 2..16 }
+
+  validates :password,
+            :presence => true,
+            :length   => { :within => 6..20 }
+
+  validates :email,   
+            :presence => true,   
+            :uniqueness => true,   
+            :format => { :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i }  
 end
+
