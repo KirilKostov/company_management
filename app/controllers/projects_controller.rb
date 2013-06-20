d9d0ed6d760254a437404dc9b9eb6d857f2394f1
KirilKostov <kiril_lol@abv.bg>
2013-06-20 16:59:45 +0300

    Add model, view and controller for the Project class

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -8,4 +8,36 @@ class ProjectsController < ApplicationController
     @project = Project.find(params[:id])
   end
 
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new params[:project]
+
+    if @project.save
+      redirect_to projects_path
+    else
+      render action: 'new'
+    end
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update_attributes(params[:project])
+      redirect_to projects_path
+    else
+      render action: 'edit'
+    end
+  end
+
+  def destroy
+    Project.find(params[:id]).destroy
+    redirect_to projects_path
+  end
+
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,9 @@
 class Project < ActiveRecord::Base
   attr_accessible :name, :description
+
+  #validations
+  validates :name,
+            :presence => true
+  validates :description,
+            :presence => true
 end
