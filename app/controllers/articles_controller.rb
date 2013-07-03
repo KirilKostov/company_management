d4a1e09069c7d7d9b52974203e9fbe7031a4476a
KirilKostov <kiril_lol@abv.bg>
2013-07-03 17:50:16 +0300

    Add general Articles view and controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,47 +1,11 @@
 class ArticlesController < ApplicationController
 
-  before_filter do
-    @user = User.find(params[:user_id])
+  def index
+    @articles = Article.all
   end
 
   def show
     @article = Article.find(params[:id])
-  end
-
-  def new
-    @article = Article.new
-  end
-
-  def create
-    @article = Article.new params[:article]
-
-    if @article.save
-
-      @user.articles << @article
-
-      redirect_to user_path(@user)
-    else
-      render action: 'new'
-    end
-  end
-
-
-  def edit
-    @article = Article.find(params[:id])
-  end
-
-  def update
-    @article = Article.find(params[:id])
-    if @article.update_attributes(params[:article])
-      redirect_to user_path(@user)
-    else
-      render action: 'edit'
-    end
-  end
-
-  def destroy
-    Article.find(params[:id]).destroy
-    redirect_to user_path(@user)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ CompanyManagement::Application.routes.draw do
   resources :projects
   resources :users do
     resources :contacts, :except => [:index]
-    resources :articles, :except => [:index]
+    resources :articles, :controller => 'users/articles'
   end
   resources :sessions, only: [:new, :create, :destroy]
+  resources :articles
 end
